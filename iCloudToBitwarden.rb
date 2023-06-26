c48488e5f790034d341c13c5b4a72c062b98aebc
yingchao <77818186+baggiiiie@users.noreply.github.com>
2023-06-26 19:45:17 +0800

    Add files via upload

---
+++ iCloudToBitwarden.rb
@@ -0,0 +1,10 @@
+require 'csv'
+
+CSV.open('bitwarden.csv', 'w') do |csv|
+  csv << ['folder', 'favorite', 'type', 'name', 'notes', 'fields', 'login_uri', 'login_username', 'login_password', 'login_totp']
+
+  CSV.open('Passwords.csv', headers: true).each do |row|
+    data = ['', 0, 'login', row['Title'], '', '', row['Login URL'], row['Login Username'], row['Login Password'], '']
+    csv << data
+  end
+end
